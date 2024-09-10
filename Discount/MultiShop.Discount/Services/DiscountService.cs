2710e9d3f823268768fbef03c998f63d660d4452
Furkan Berk AKDAŞ <frknbrk66@hotmail.com>
2024-09-10 10:56:37 +0300

    İndirim (Discount) ve Sipariş (Order) Servisleri Eklendi

---
+++ Discount/MultiShop.Discount/Services/DiscountService.cs
@@ -0,0 +1,79 @@
+﻿using Dapper;
+using MultiShop.Discount.Context;
+using MultiShop.Discount.Dtos;
+using System.Data;
+
+namespace MultiShop.Discount.Services
+{
+    public class DiscountService : IDiscountService
+    {
+        private readonly DapperContext _context;
+
+        public DiscountService(DapperContext dapperContext)
+        {
+            _context = dapperContext;
+        }
+
+        public async Task CreateDiscountCouponAsync(CreateDiscountCouponDto createCouponDto)
+        {
+            string query = "insert into Coupons (Code,Rate,IsActive,ValidDate) values (@code,@rate,@isactive,@validdate)";
+            var parameters = new DynamicParameters();
+            parameters.Add("@code", createCouponDto.Code);
+            parameters.Add("@rate", createCouponDto.Rate);
+            parameters.Add("@isactive",createCouponDto.IsActive);
+            parameters.Add("@validdate", createCouponDto.ValidDate);
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
+        public async Task DeleteDiscountCouponAsync(int couponId)
+        {
+            string query = "Delete From Coupons where CouponId= @couponid";
+            var parameters = new DynamicParameters();
+            parameters.Add("@couponid", couponId);
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
+        public async Task<List<ResultDiscountCouponDto>> GetAllDiscountCouponAsync()
+        {
+            string query = "Select * From Coupons";
+            using(var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultDiscountCouponDto>(query);
+                return values.ToList();
+            }
+        }
+
+        public async Task<GetByIdDiscountCouponDto> GetByIdDiscountCouponAsync(int couponId)
+        {
+            string query = "Select * From Coupons where CouponId = @couponid";
+            var parameters = new DynamicParameters();
+            parameters.Add("@couponid",couponId);
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryFirstOrDefaultAsync<GetByIdDiscountCouponDto>(query,parameters);
+                return values;
+            }
+        }
+
+        public async Task UpdateDiscountCouponAsync(UpdateDiscountCouponDto updateCouponDto)
+        {
+            string query = "Update Coupons Set Code=@code , Rate=@rate, IsActive = @isactive , ValidDate = @validdate where CouponId= @couponıd ";
+            var parameters = new DynamicParameters();
+            parameters.Add("@couponıd", updateCouponDto.CouponId);
+            parameters.Add("@code", updateCouponDto.Code);
+            parameters.Add("@rate", updateCouponDto.Rate);
+            parameters.Add("@isactive", updateCouponDto.IsActive);
+            parameters.Add("@validdate", updateCouponDto.ValidDate);
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+    }
+}
---
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Controllers/AddressesController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+using MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
+using MultiShop.Order.Application.Features.CQRS.Queries.AddressQueries;
+
+namespace MultiShop.Order.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressesController : ControllerBase
+    {
+        private readonly GetAddressQueryHandler _getAddressQueryHandler;
+        private readonly GetAddressByIdQueryHandler _getAddressByIdQueryHandler;
+        private readonly CreateAddressCommanHandler _createAddressCommanHandler;
+        private readonly UpdateAddressComandHandler _updateAddressComandHandler;
+        private readonly RemoveAddressCommandHandler _removeAddressCommandHandler;
+        public AddressesController(GetAddressQueryHandler getAddressQueryHandler, GetAddressByIdQueryHandler getAddressByIdQueryHandler, CreateAddressCommanHandler createAddressCommanHandler, UpdateAddressComandHandler updateAddressComandHandler, RemoveAddressCommandHandler removeAddressCommandHandler)
+        {
+            _getAddressQueryHandler = getAddressQueryHandler;
+            _getAddressByIdQueryHandler = getAddressByIdQueryHandler;
+            _createAddressCommanHandler = createAddressCommanHandler;
+            _updateAddressComandHandler = updateAddressComandHandler;
+            _removeAddressCommandHandler = removeAddressCommandHandler;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddressList()
+        {
+            var values = _getAddressQueryHandler.Handle();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> AddressListById(int id)
+        {
+            var values = _getAddressByIdQueryHandler.Handle(new GetAddressByIdQuery(id));
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAddress(CreateAddressCommand command)
+        {
+            await _createAddressCommanHandler.Handle(command);
+            return  Ok("Adres bilgisi başarıyla eklendi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAddress(UpdateAddressCommand command)
+        {
+            await _updateAddressComandHandler.Handle(command);
+            return Ok("Adres bilgisi başarıyla güncellendi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoveAddress(int id)
+        {
+            await _removeAddressCommandHandler.Handle(new RemoveAddressCommand(id));
+            return Ok("Adres başarıyla silindi");
+        }
+    }
+}
