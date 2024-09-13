7668c8a48bbc9947d2835457fa15cbeced8a71a0
Furkan Berk AKDAŞ <frknbrk66@hotmail.com>
2024-09-13 16:43:31 +0300

    Order Service  CQRS yapısı ile Oluşturuldu - IdentityServer kuruldu Auth2.0 ile token alındı

--- Services/Order/Infrastructure/MultiShop.Order.Persistence/Context/OrderContext.cs
+++ Services/Order/Infrastructure/MultiShop.Order.Persistence/Context/OrderContext.cs
@@ -12,7 +12,7 @@ namespace MultiShop.Order.Persistence.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=...;initial Catalog=MultiShopOrderDb;integrated Security=true;TrustServerCertificate=True;");
+            optionsBuilder.UseSqlServer("Server=localhost,1440;initial Catalog=MultiShopOrderDb;User=sa;Password=102027242611Fba.;integrated Security=true;Trusted_connection=false;TrustServerCertificate=True");
         }
 
         public DbSet<Address> Addresses{ get; set; }
